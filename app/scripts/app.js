17ff8d14f202de7883bcf0745ead95f359c7656e
Felix <felix@stkn.org>
2014-04-14 19:57:41 +0200

    Add d3js lib

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,8 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'nvd3ChartDirectives'
   ])
   .config(function ($routeProvider) {
     $routeProvider
