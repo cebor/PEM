99c875a97d2847911ff34463f32f9a2f85fcc832
Felix <felix@stkn.org>
2014-05-06 21:21:55 +0200

    Add factory for news feeds

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,4 +1,5 @@
 'use strict';
 
 angular.module('stockApp')
-  .constant('YAHOO_API', 'http://query.yahooapis.com/v1/public/yql');
+  .constant('YAHOO_API', 'http://query.yahooapis.com/v1/public/yql')
+  .constant('GOOGLE_FEED_API', 'http://ajax.googleapis.com/ajax/services/feed/load');
