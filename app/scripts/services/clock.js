34f7a494cd391a9f26ec028b06ba5e12d075752a
Felix <felix@stkn.org>
2014-05-14 14:11:23 +0200

    Don't touch a snippet!

--- app/scripts/services/clock.js
+++ app/scripts/services/clock.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* jshint ignore:start */
 angular.module('stockApp')
   .service('Clock', function Clock() {
 
@@ -175,11 +176,12 @@ angular.module('stockApp')
         easeOutElastic: function (x, t, b, c, d) {
           var s=1.70158;var p=0;var a=c;
           if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-          if (a < Math.abs(c)) { a=c; s=p/4; }
-          else s = p/(2*Math.PI) * Math.asin (c/a);
+          if (a < Math.abs(c)) { a=c; var s=p/4; }
+          else var s = p/(2*Math.PI) * Math.asin (c/a);
           return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
         }
       });
     };
 
 });
+/* jshint ignore:end */
