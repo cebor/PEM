a46e5c05a29788a69f2b97ea9730308f54b66489
Felix <felix@stkn.org>
2014-05-14 13:49:08 +0200

    Fix Zoom cpu load issues

--- app/scripts/services/zoom.js
+++ app/scripts/services/zoom.js
@@ -1,82 +1,34 @@
 'use strict';
 
 angular.module('stockApp')
-  .service('Zoom', function Zoom($interval, $timeout) {
+  .service('Zoom', function Zoom($interval, $timeout, chartXAxis) {
 
-    var ZOOM_IN_SPEED = 5;
+    var month = 30 * 24 * 3600 * 1000;
 
-    var intervals = [];
-    var timeouts = [];
+    var interval;
 
     this.start = function (startDate, endDate, range) {
 
-      if (!intervals.length) {
+      if (!interval) {
 
-        var firstDate = new Date();
-        firstDate.setMonth(endDate.getMonth() - 1);
-        var midDate = new Date();
-        midDate.setMonth(endDate.getMonth() - Math.floor(range / 2));
+        var min = new Date(startDate).getTime() + Math.floor(range / 3) * month;
 
-        intervals[0] = $interval(function () {
-          var tmpDate = new Date(startDate.getTime());
+        chartXAxis.currentMax = min;
 
-          timeouts[0] = $timeout(function () {
-            intervals[1] = $interval(function () {
-              if (tmpDate.setDate(tmpDate.getDate() + ZOOM_IN_SPEED) <= firstDate) {
+        interval = $interval(function () {
+          chartXAxis.currentMax += month;
 
-                Highcharts.charts[0].xAxis[0].setExtremes(
-                  tmpDate.getTime(),
-                  endDate.getTime(),
-                  true,
-                  false
-                );
-
-              } else {
-                tmpDate = firstDate;
-                $interval.cancel(intervals[1]);
-                Highcharts.charts[0].xAxis[0].setExtremes(
-                  tmpDate.getTime(),
-                  endDate.getTime(),
-                  true,
-                  true
-                );
-              }
-            }, 1);
-          }, 1000);
-
-          timeouts[1] = $timeout(function () {
-            Highcharts.charts[0].xAxis[0].setExtremes(
-              midDate.getTime(),
-              endDate.getTime()
-            );
-          }, 4000);
-
-          timeouts[2] = $timeout(function () {
-            Highcharts.charts[0].xAxis[0].setExtremes(
-              startDate.getTime(),
-              endDate.getTime(),
-              true,
-              true
-            );
-          }, 6000);
-
-        }, 7000);
+          if (chartXAxis.currentMax >= new Date(endDate).getTime()) {
+            chartXAxis.currentMax = min;
+          }
+        }, 5000);
 
       }
 
     };
 
     this.stop = function () {
-      angular.forEach(intervals, function (interval) {
-        $interval.cancel(interval);
-      });
-
-      angular.forEach(timeouts, function (timeout) {
-        $timeout.cancel(timeout);
-      });
-
-      intervals = [];
-      timeouts = [];
+      $interval.cancel(interval);
     };
 
   });
