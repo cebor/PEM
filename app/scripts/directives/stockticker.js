2d4dd0483893090051e948951931a30395260bbd
Felix <felix@stkn.org>
2014-05-26 20:59:53 +0200

    Move stockTocker logic to directive

--- app/scripts/directives/stockticker.js
+++ app/scripts/directives/stockticker.js
@@ -1,7 +1,15 @@
 'use strict';
 
 angular.module('stockApp')
-  .directive('stockTicker', function () {
+  .directive('stockTicker', function (
+    $filter,
+    $timeout,
+    chartXAxis,
+    PieChartConfig,
+    StockChartConfig,
+    stockData,
+    Zoom
+  ) {
     return {
       replace: true,
       restrict: 'E',
@@ -9,25 +17,67 @@ angular.module('stockApp')
         config: '='
       },
       templateUrl: 'views/stockticker.html',
-      controller: ['$scope', '$timeout', 'Zoom', function ($scope, $timeout, Zoom) {
-        var startDate = $scope.config.startDate;
-        var endDate = $scope.config.endDate;
+      link: function (scope) {
+
+        /* config */
+
+        var startDate = scope.config.startDate;
+        var endDate = scope.config.endDate;
+
+        var stockStartDate = $filter('date')(startDate, 'yyyy-MM-dd');
+        var stockEndDate = $filter('date')(endDate, 'yyyy-MM-dd');
+
+        scope.slides = [];
+        var titles = [];
+
+
+        /* get data */
+
+        angular.forEach(scope.config.symbols, function (value, key) {
+
+          titles.push(value.join(', '));
+
+          var slide = {
+            stockChartConfig: new StockChartConfig(),
+            pieChartConfig: new PieChartConfig()
+          };
+
+          this[key] = slide;
+
+          var that = this;
+
+          stockData.get(value, stockStartDate, stockEndDate).then(function (data) {
+            that[key].stockChartConfig.series = data;
+            that[key].stockChartConfig.xAxis = chartXAxis;
+            that[key].stockChartConfig.title.text = value.join(', ');
+            that[key].stockChartConfig.loading = false;
+            that[key].pieChartConfig.series.push(stockData.getPie(data));
+            that[key].pieChartConfig.loading = false;
+          });
+
+        }, scope.slides);
+
+        scope.title = titles.join(' - ');
+
+
+        /* zoom */
 
         Zoom.start(startDate, endDate);
 
-        $scope.zoomStart = function () {
+        scope.zoomStart = function () {
           $timeout(function () {
             Zoom.start(startDate, endDate);
           }, 3000);
         };
 
-        $scope.zoomStop = function () {
+        scope.zoomStop = function () {
           Zoom.stop();
         };
 
-        $scope.$on('$destroy', function () {
+        scope.$on('$destroy', function () {
           Zoom.stop();
         });
-      }]
+
+      }
     };
   });
